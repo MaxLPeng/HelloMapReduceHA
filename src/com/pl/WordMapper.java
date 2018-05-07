50c6110a7a841bd27a0cbe9f87f1bad75f7f4cba
MaxLPeng <pengliang_sun@hotmail.com>
2018-05-07 20:15:00 +0800

    test stash

--- src/com/pl/WordMapper.java
+++ src/com/pl/WordMapper.java
@@ -11,17 +11,17 @@ public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
 
 	@Override
 	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
-			throws IOException, InterruptedException {
-			
-			final IntWritable oneWord = new IntWritable(1); 
-			String lineInfo = value.toString();
-			System.out.println("WordMapper:map()------------------");
-			System.out.println("Input:key(LongWritable)=" + key + " value(Text)=" + value);
-			String[] words = lineInfo.split(" ");
-			for(String word : words) {
-				System.out.println("output:key(Text)=" + word + " value(IntWritable)=" + oneWord);
-	            context.write(new Text(word), oneWord);
-	        }
+		throws IOException, InterruptedException {
+		
+		final IntWritable oneWord = new IntWritable(1); 
+		String lineInfo = value.toString();
+		System.out.println("WordMapper:map()------------------");
+		System.out.println("Input:key(LongWritable)=" + key + " value(Text)=" + value);
+		String[] words = lineInfo.split(" ");
+		for(String word : words) {
+			System.out.println("output:key(Text)=" + word + " value(IntWritable)=" + oneWord);
+	        context.write(new Text(word), oneWord);
+	    }
 	}
 
 	
--- src/com/pl/WordReducer.java
+++ src/com/pl/WordReducer.java
@@ -11,18 +11,18 @@ public class WordReducer extends Reducer<Text, IntWritable, Text, LongWritable> 
 
 	@Override
 	protected void reduce(Text key, Iterable<IntWritable> values,
-			Reducer<Text, IntWritable, Text, LongWritable>.Context context) throws IOException, InterruptedException {
-			String substr = "";
-			long count = 0;
-			for	(IntWritable v:values) {
-					count +=v.get();  
-					substr += "," + v.get();
-			}
-			System.out.println("WordReducer:reduce()------------------");
-			System.out.println("Input:key(Text)=" + key + " value(IntWritable[])=" + substr);			
-			System.out.println("Output:key(Text)=" + key + " value(LongWritable)=" + count);
-			
-			context.write(key, new LongWritable(count));
+		Reducer<Text, IntWritable, Text, LongWritable>.Context context) throws IOException, InterruptedException {
+		String substr = "";
+		long count = 0;
+		for	(IntWritable v:values) {
+				count +=v.get();  
+				substr += "," + v.get();
+		}
+		System.out.println("WordReducer:reduce()------------------");
+		System.out.println("Input:key(Text)=" + key + " value(IntWritable[])=" + substr);			
+		System.out.println("Output:key(Text)=" + key + " value(LongWritable)=" + count);
+		
+		context.write(key, new LongWritable(count));
 	}
 
 }
